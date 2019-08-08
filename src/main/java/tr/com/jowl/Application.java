d321f995859966960d821d7a9e7f321762362897
BharathKumarRV <44936774+BharathKumarRV@users.noreply.github.com>
2019-08-08 15:49:28 +0530

    Update Application.java

--- src/main/java/tr/com/jowl/Application.java
+++ src/main/java/tr/com/jowl/Application.java
@@ -16,7 +16,7 @@ import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
  * @version 1.0
  * Date 4/27/2018.
  */
-@Configuration
+
 @ComponentScan(basePackages = {"tr.com"})
 @SpringBootApplication
 public class Application implements WebMvcConfigurer {
